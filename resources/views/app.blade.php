3df03ac58923f6d97992618d68057cda0cf56412
Rifqi Munawar <rifqimunawar48@gmail.com>
2024-03-22 23:31:29 +0700

    manifest error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
+        {{-- @vite('resources/css/app.css') --}}
+        @vite{{ asset('css/app.css') }}
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
