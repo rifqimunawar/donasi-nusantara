474c58fa7f2998709ce4bb9c4fff27bec4f0b853
Rifqi Munawar <rifqimunawar48@gmail.com>
2024-03-23 02:53:53 +0700

    rincian

--- public/build/assets/Aboutpage-B2kncym8.js
+++
@@ -1,1 +0,0 @@
-import{j as e,d as r}from"./app-BGbLhe-4.js";function s(){return e.jsxs("div",{children:["Aboutpage",e.jsx(r,{href:"/home",className:"btn btn-primary",children:"Home"})]})}export{s as default};
