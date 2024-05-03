5c8284f88ebeff292c5bdd49eb3f7a977b182a84
Rifqi Munawar <rifqimunawar48@gmail.com>
2024-05-03 15:13:37 +0700

    add aboutpage

--- resources/js/Components/BottomNavbar.jsx
+++ resources/js/Components/BottomNavbar.jsx
@@ -4,7 +4,54 @@ import React from "react";
 export default function BottomNavbar() {
     return (
         <div className="fixed bottom-1 flex w-full container">
-            <div className="grid grid-cols-4 gap-3 mx-auto max-w-screen-lg">
+            <div className="btm-nav grid grid-cols-4 mx-auto max-w-screen-sm">
+                <Link href="/home">
+                    <button
+                        className={
+                            window.location.pathname === "/home"
+                                ? "text-primary active"
+                                : "text-secondary"
+                        }
+                    >
+                        <i className="bi bi-house"></i>
+                    </button>
+                </Link>
+                <Link href="/about">
+                    <button
+                        className={
+                            window.location.pathname === "/about"
+                                ? "text-primary active"
+                                : "text-secondary"
+                        }
+                    >
+                        <i className="bi bi-info-circle"></i>
+                    </button>
+                </Link>
+                <Link href="/list">
+                    <button
+                        className={
+                            window.location.pathname === "/list"
+                                ? "text-primary active"
+                                : "text-secondary"
+                        }
+                    >
+                        <i className="bi bi-list-ul"></i>
+                    </button>
+                </Link>
+                <Link href="/profile">
+                    <button
+                        className={
+                            window.location.pathname === "/profile"
+                                ? "text-primary active"
+                                : "text-secondary"
+                        }
+                    >
+                        <i className="bi bi-person-circle"></i>
+                    </button>
+                </Link>
+            </div>
+
+            {/* <div className="grid grid-cols-4 gap-3 mx-auto max-w-screen-lg">
                 <Link href={'/home'} className="btn btn-primary w-full">
                     <i class="bi bi-house"></i>
                     <p className="text-xs">Home</p>
@@ -21,7 +68,7 @@ export default function BottomNavbar() {
                     <i class="bi bi-person-circle"></i>
                     <p className="text-xs">Profile</p>
                 </Link>
-            </div>
+            </div> */}
         </div>
     );
 }
---
+++ resources/js/Pages/frontend/AboutPage.jsx
@@ -0,0 +1,56 @@
+import BottomNavbar from "@/Components/BottomNavbar";
+import FooterComponent from "@/Components/FooterComponent";
+import NavbarComponent from "@/Components/NavbarComponent";
+import { Head } from "@inertiajs/react";
+import React from "react";
+
+export default function AboutPage({ masterImageUrl }) {
+    return (
+        <section className="my-0 mx-auto min-h-full max-w-screen-sm">
+            <div className="my-0 mx-auto min-h-screen max-w-480 overflow-x-hidden pb-[66px]">
+                <Head title="About" />
+                <div>
+                    <NavbarComponent />
+                </div>
+                <div className="my-8 text-center">
+                    <p className="text-2xl">About Us</p>
+                </div>
+                <div className="h-96 mb-5">
+                    <img
+                        src={`${masterImageUrl}sempro1.jpg`}
+                        className="w-full h-full object-cover rounded-3xl shadow"
+                    />
+                </div>
+                <div className="text-lg text-justify">
+                    <p>
+                        Website ini merupakan hasil dari upaya kelompok 8 dalam
+                        menyelesaikan tugas akhir perkuliahan pada Program Studi
+                        Teknik Informatika. Kami, Delaisa Lisyami, Endi Rahman,
+                        Ilyas Khairul Alimnis, Melyta Contesa, dan Rifqi
+                        Munawar, dengan rendah hati menyadari bahwa website kami
+                        belum mencapai kesempurnaan yang Anda harapkan. Kami
+                        mengakui bahwa terdapat kekurangan dan keterbatasan
+                        dalam fitur yang kami tawarkan.
+                    </p>
+                    <br />
+                    <p>
+                        Namun, kami merasa terhormat karena dengan sederhananya
+                        website ini, kami telah berhasil melalui tahap seminar
+                        proposal pada tanggal 24 April 2024. Kami memohon doa
+                        dan dukungan agar kami dapat menyelesaikan studi kami
+                        dengan baik, dan kami akan terus berkomitmen untuk
+                        mengembangkan website ini guna memberikan pengalaman
+                        yang lebih baik bagi pengunjung. Terima kasih atas
+                        segala dukungan dan kesempatan yang diberikan!
+                    </p>
+                </div>
+                <div>
+                    <FooterComponent />
+                </div>
+                <div className="btm-center">
+                    <BottomNavbar />
+                </div>
+            </div>
+        </section>
+    );
+}
